5e4b3301981c3f3de263671e182f51bade53ef3c
jinwook song <wlsdnr129@naver.com>
2025-01-28 15:04:06 +0900

    style(card): ideasGPT card

--- app/common/pages/home-page.tsx
+++ app/common/pages/home-page.tsx
@@ -2,6 +2,7 @@ import { Link, type MetaFunction } from 'react-router';
 import { ProductCard } from '~/features/products/components/product-card';
 import { Button } from '../components/ui/button';
 import { PostCard } from '~/features/community/components/post-card';
+import { IdeaCard } from '~/features/ideas/components/idea-card';
 
 export const meta: MetaFunction = () => {
   return [
@@ -60,6 +61,30 @@ export default function HomePage() {
             authorAvatarUrl='https://github.com/apple.png'
             category='Productivity'
             createdAt='12 hours ago'
+          />
+        ))}
+      </div>
+      <div className='grid grid-cols-3 gap-4'>
+        <div>
+          <h2 className='text-5xl font-bold leading-tight tracking-tight'>
+            IdeasGPT
+          </h2>
+          <p className='text-xl font-light text-foreground'>
+            Find ideas for your next project
+          </p>
+          <Button variant={'link'} asChild className='text-lg p-0'>
+            <Link to={'/community'}>Explore all discussions &rarr;</Link>
+          </Button>
+        </div>
+        {Array.from({ length: 10 }).map((_, index) => (
+          <IdeaCard
+            key={index}
+            id='ideaId'
+            title='A startup that creates an AI-powered generated personal trainer, delivering customized fitness recommendations and tracking of progress using a mobile app to track workouts and progress as well as a web dashboard to track progress and see insights.'
+            viewCount={123}
+            createdAt='12 hours ago'
+            likesCount={12}
+            claimed={index % 2 === 0}
           />
         ))}
       </div>
--- app/features/community/components/post-card.tsx
+++ app/features/community/components/post-card.tsx
@@ -11,6 +11,7 @@ import {
   AvatarImage,
 } from '~/common/components/ui/avatar';
 import { Button } from '~/common/components/ui/button';
+import { DotIcon } from 'lucide-react';
 
 interface PostCardProps {
   id: string;
@@ -40,16 +41,17 @@ export function PostCard({
           <div className='space-y-2'>
             <CardTitle>{title}</CardTitle>
             <div className='flex gap-2 text-sm leading-tight text-muted-foreground'>
-              <span>{author} on</span>
-              <span>{category}</span>
-              <span>•</span>
+              <span>
+                {author} on {category}
+              </span>
+              <DotIcon className='size-4' />
               <span>{createdAt}</span>
             </div>
           </div>
         </CardHeader>
         <CardFooter className='flex justify-end'>
           <Button variant={'link'} asChild>
-            <Link to={`/community/${id}`}>Reply &rarr;</Link>
+            Reply &rarr;
           </Button>
         </CardFooter>
       </Card>
