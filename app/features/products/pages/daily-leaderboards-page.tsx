98c3d57b832088e7ff7a5a8d283c3103406cf79f
jinwook song <wlsdnr129@naver.com>
2025-01-29 00:24:41 +0900

    feat(validate): date with zod, luxon

--- app/features/products/pages/daily-leaderboards-page.tsx
+++ app/features/products/pages/daily-leaderboards-page.tsx
@@ -1,33 +1,66 @@
-import type { Route } from '~/types';
-import type { MetaFunction } from 'react-router';
+import { DateTime } from 'luxon';
+import type { Route } from './+types/daily-leaderboards-page';
+import { data, isRouteErrorResponse } from 'react-router';
+import { z } from 'zod';
 
-export const meta: MetaFunction = () => {
-  return [
-    { title: 'Daily Leaderboard | Product Hunt Clone' },
-    { name: 'description', content: 'Best products of the day' },
-  ];
-};
+const paramsSchema = z.object({
+  year: z.coerce.number(),
+  month: z.coerce.number(),
+  day: z.coerce.number(),
+});
 
-export function loader({ request, params }: Route.LoaderArgs) {
-  return {
-    year: params.year,
-    month: params.month,
-    day: params.day,
-    products: [],
-  };
+export function loader({ params }: Route.LoaderArgs) {
+  const { success, data: parsedData } = paramsSchema.safeParse(params);
+
+  if (!success) {
+    throw data(
+      { message: 'Invalid params', error_code: 'INVALID_PARAMS' },
+      { status: 400 },
+    );
+  }
+
+  const { year, month, day } = parsedData;
+  const date = DateTime.fromObject({ year, month, day }).setZone('Asia/Seoul');
+
+  if (!date.isValid) {
+    throw data(
+      { message: 'Invalid date', error_code: 'INVALID_DATE' },
+      { status: 400 },
+    );
+  }
+
+  const today = DateTime.now().setZone('Asia/Seoul').startOf('day');
+  if (date > today) {
+    throw data(
+      { message: 'Future date', error_code: 'FUTURE_DATE' },
+      { status: 400 },
+    );
+  }
+
+  return { date };
 }
 
 export default function DailyLeaderboardsPage({
   loaderData,
 }: Route.ComponentProps) {
-  const { year, month, day, products } = loaderData;
+  // const { year, month, day, products } = loaderData;
 
-  return (
-    <main className='container mx-auto px-4 py-8'>
-      <h1 className='text-3xl font-bold mb-6'>
-        Top Products of {month}/{day}/{year}
-      </h1>
-      {/* Daily leaderboard content will be rendered here */}
-    </main>
-  );
+  return <main className='container mx-auto px-4 py-8'></main>;
+}
+
+export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
+  // loader/action에서 발생한 (4xx, 5xx) 에러
+  if (isRouteErrorResponse(error)) {
+    return (
+      <div>
+        {error.data.message} / {error.data.error_code}
+      </div>
+    );
+  }
+
+  if (error instanceof Error) {
+    return <div>{error.message}</div>;
+  }
+
+  return <div>Unknwon error</div>;
 }
