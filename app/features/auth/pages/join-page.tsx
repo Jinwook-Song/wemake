fb7e7bad31abae7c67e2a985e1e347ba5529742a
jinwook song <wlsdnr129@naver.com>
2025-02-01 18:40:22 +0900

    style(ui): login, join page

--- app/features/auth/pages/join-page.tsx
+++ app/features/auth/pages/join-page.tsx
@@ -1,4 +1,7 @@
+import { Form, Link } from 'react-router';
 import type { Route } from './+types/join-page';
+import { InputPair } from '~/common/components/input-pair';
+import { Button } from '~/common/components/ui/button';
 
 export const meta: Route.MetaFunction = () => {
   return [
@@ -8,5 +11,55 @@ export const meta: Route.MetaFunction = () => {
 };
 
 export default function JoinPage({}: Route.ComponentProps) {
-  return <div></div>;
+  return (
+    <div className='flex flex-col justify-center items-center h-full relative'>
+      <Button variant='ghost' asChild className='absolute top-8 right-8'>
+        <Link to='/auth/login'>Login</Link>
+      </Button>
+      <div className='flex flex-col justify-center items-center gap-10 w-full max-w-md'>
+        <h1 className='text-2xl font-semibold'>Create an account</h1>
+        <Form className='w-full space-y-4'>
+          <InputPair
+            id='name'
+            name='name'
+            label='Name'
+            description='Enter your name'
+            type='text'
+            placeholder='Enter your name'
+            required
+          />
+          <InputPair
+            id='username'
+            name='username'
+            label='Username'
+            description='Enter your username'
+            type='text'
+            placeholder='Enter your username'
+            required
+          />
+          <InputPair
+            id='email'
+            name='email'
+            label='Email'
+            description='Enter your email'
+            type='email'
+            placeholder='Enter your email'
+            required
+          />
+          <InputPair
+            id='password'
+            name='password'
+            label='Password'
+            description='Enter your password'
+            type='password'
+            placeholder='Enter your password'
+            required
+          />
+          <Button type='submit' className='w-full'>
+            Create account
+          </Button>
+        </Form>
+      </div>
+    </div>
+  );
 }
--- app/features/auth/pages/login-page.tsx
+++ app/features/auth/pages/login-page.tsx
@@ -1,4 +1,7 @@
+import { Button } from '~/common/components/ui/button';
 import type { Route } from './+types/login-page';
+import { InputPair } from '~/common/components/input-pair';
+import { Form, Link } from 'react-router';
 
 export const meta: Route.MetaFunction = () => {
   return [
@@ -8,5 +11,37 @@ export const meta: Route.MetaFunction = () => {
 };
 
 export default function LoginPage({}: Route.ComponentProps) {
-  return <div></div>;
+  return (
+    <div className='flex flex-col justify-center items-center h-full relative'>
+      <Button variant='ghost' asChild className='absolute top-8 right-8'>
+        <Link to='/auth/join'>Join</Link>
+      </Button>
+      <div className='flex flex-col justify-center items-center gap-10 w-full max-w-md'>
+        <h1 className='text-2xl font-semibold'>Log in to wemake</h1>
+        <Form className='w-full space-y-4'>
+          <InputPair
+            id='email'
+            name='email'
+            label='Email'
+            description='Enter your email'
+            type='email'
+            placeholder='Enter your email'
+            required
+          />
+          <InputPair
+            id='password'
+            name='password'
+            label='Password'
+            description='Enter your password'
+            type='password'
+            placeholder='Enter your password'
+            required
+          />
+          <Button type='submit' className='w-full'>
+            Log in
+          </Button>
+        </Form>
+      </div>
+    </div>
+  );
 }
