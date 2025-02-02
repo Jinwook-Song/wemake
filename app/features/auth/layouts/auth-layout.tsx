70bc252ce0796a6662d4b7f4298ea0338b373d3c
jinwook song <wlsdnr129@naver.com>
2025-02-02 21:31:55 +0900

    style(ui): magic ui

--- app/features/auth/layouts/auth-layout.tsx
+++ app/features/auth/layouts/auth-layout.tsx
@@ -1,5 +1,6 @@
 import { Outlet } from 'react-router';
 import type { Route } from './+types/auth-layout';
+import { FlickeringGrid } from '~/common/components/ui/flickering-grid';
 
 export const meta: Route.MetaFunction = () => {
   return [
@@ -11,7 +12,17 @@ export const meta: Route.MetaFunction = () => {
 export default function AuthLayout({}: Route.ComponentProps) {
   return (
     <div className='grid lg:grid-cols-2 h-screen px-5 sm:px-0'>
-      <div className='hidden lg:block bg-gradient-to-br from-primary/80 via-black to-primary/10' />
+      {/* <div className='hidden lg:block bg-gradient-to-br from-primary/80 via-black to-primary/10' /> */}
+      <div className='hidden lg:block'>
+        <FlickeringGrid
+          className='relative inset-0 z-0 [mask-image:radial-gradient(300px_circle_at_center,white,transparent)]'
+          squareSize={4}
+          gridGap={6}
+          color='#E11D49'
+          maxOpacity={0.8}
+          flickerChance={0.1}
+        />{' '}
+      </div>
       <Outlet />
     </div>
   );
