4bd5636f282509862d69ca0ed251ec5df0857bee
jinwook song <wlsdnr129@naver.com>
2025-03-16 14:31:50 +0900

    feat(auth): social login (github, kakao)

--- app/features/auth/pages/social-start-page.tsx
+++ app/features/auth/pages/social-start-page.tsx
@@ -1,12 +1,32 @@
+import { z } from 'zod';
 import type { Route } from './+types/social-start-page';
+import { redirect } from 'react-router';
+import { makeSSRClient } from '~/supa-client';
 
-export const meta: Route.MetaFunction = () => {
-  return [
-    { title: 'Social Login | wemake' },
-    { name: 'description', content: 'Start social login process' },
-  ];
+const paramSchema = z.object({
+  provider: z.enum(['github', 'kakao']),
+});
+
+export const loader = async ({ params, request }: Route.LoaderArgs) => {
+  const { success, data } = paramSchema.safeParse(params);
+  if (!success) {
+    return redirect('/auth/login');
+  }
+
+  const { provider } = data;
+  const redirectTo = `http://localhost:5173/auth/social/${provider}/complete`;
+  const { client, headers } = makeSSRClient(request);
+  const {
+    data: { url },
+    error,
+  } = await client.auth.signInWithOAuth({
+    provider,
+    options: { redirectTo },
+  });
+
+  if (error) throw error;
+
+  // 쿠키를 포함한 헤더를 전달하여 인증 세션을 유지합니다.
+  // 소셜 로그인 후 사용자가 돌아올 때 인증 상태를 유지하기 위함입니다.
+  if (url) return redirect(url, { headers });
 };
-
-export default function SocialStartPage({}: Route.ComponentProps) {
-  return <div></div>;
-}
