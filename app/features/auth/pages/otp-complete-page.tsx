c627463251b465780cefd6d44cfcedac5d6023e1
jinwook song <wlsdnr129@naver.com>
2025-03-16 15:11:44 +0900

    feat(auth): otp login with email

--- app/features/auth/pages/otp-complete-page.tsx
+++ app/features/auth/pages/otp-complete-page.tsx
@@ -1,7 +1,10 @@
-import { Form } from 'react-router';
+import { Form, redirect, useNavigation, useSearchParams } from 'react-router';
 import type { Route } from './+types/otp-complete-page';
 import { InputPair } from '~/common/components/input-pair';
 import { Button } from '~/common/components/ui/button';
+import { z } from 'zod';
+import { makeSSRClient } from '~/supa-client';
+import { Loader } from 'lucide-react';
 
 export const meta: Route.MetaFunction = () => {
   return [
@@ -10,7 +13,42 @@ export const meta: Route.MetaFunction = () => {
   ];
 };
 
-export default function OtpCompletePage({}: Route.ComponentProps) {
+const formSchema = z.object({
+  email: z.string().email({ message: 'Invalid email address' }),
+  otp: z.string().length(6, { message: 'OTP must be 6 digits' }),
+});
+
+export const action = async ({ request }: Route.ActionArgs) => {
+  const formData = await request.formData();
+  const { success, data, error } = formSchema.safeParse(
+    Object.fromEntries(formData),
+  );
+
+  if (!success) {
+    return { fieldErrors: error.flatten().fieldErrors, verifyError: null };
+  }
+
+  const { email, otp } = data;
+  const { client, headers } = makeSSRClient(request);
+
+  const { error: verifyError } = await client.auth.verifyOtp({
+    email,
+    token: otp,
+    type: 'email',
+  });
+  if (verifyError)
+    return { verifyError: verifyError.message, fieldErrors: null };
+
+  return redirect('/', { headers });
+};
+
+export default function OtpCompletePage({ actionData }: Route.ComponentProps) {
+  const [searchParams] = useSearchParams();
+  const email = searchParams.get('email');
+  const navigation = useNavigation();
+  const isSubmitting =
+    navigation.state === 'submitting' || navigation.state === 'loading';
+
   return (
     <div className='flex flex-col justify-center items-center h-full relative'>
       <div className='flex flex-col justify-center items-center gap-10 w-full max-w-md'>
@@ -20,28 +58,47 @@ export default function OtpCompletePage({}: Route.ComponentProps) {
             Enter the OTP sent to your email
           </p>
         </div>
-        <Form className='w-full space-y-4'>
+        <Form method='post' className='w-full space-y-4'>
           <InputPair
             id='email'
             name='email'
+            defaultValue={email || ''}
             label='Email'
             description='Enter your email'
             type='email'
             placeholder='Enter your email'
             required
+            readOnly
           />
+          {actionData?.fieldErrors?.email && (
+            <p className='text-red-500 text-sm'>
+              {actionData.fieldErrors.email}
+            </p>
+          )}
           <InputPair
             id='otp'
             name='otp'
             label='OTP'
             description='Enter the OTP sent to your email'
-            type='number'
+            type='text'
             placeholder='Enter the OTP'
             required
           />
-          <Button type='submit' className='w-full'>
-            Log in
-          </Button>
+          {actionData?.fieldErrors?.otp && (
+            <p className='text-red-500 text-sm'>{actionData.fieldErrors.otp}</p>
+          )}
+          {isSubmitting ? (
+            <Button type='submit' className='w-full' disabled>
+              <Loader className='w-4 h-4 animate-spin' />
+            </Button>
+          ) : (
+            <Button type='submit' className='w-full'>
+              Log in
+            </Button>
+          )}
+          {actionData?.verifyError && (
+            <p className='text-red-500 text-sm'>{actionData.verifyError}</p>
+          )}
         </Form>
       </div>
     </div>
--- app/features/auth/pages/otp-start-page.tsx
+++ app/features/auth/pages/otp-start-page.tsx
@@ -1,7 +1,10 @@
-import { Form } from 'react-router';
+import { Form, redirect, useNavigation } from 'react-router';
 import type { Route } from './+types/otp-start-page';
 import { InputPair } from '~/common/components/input-pair';
 import { Button } from '~/common/components/ui/button';
+import { z } from 'zod';
+import { makeSSRClient } from '~/supa-client';
+import { Loader } from 'lucide-react';
 
 export const meta: Route.MetaFunction = () => {
   return [
@@ -10,7 +13,34 @@ export const meta: Route.MetaFunction = () => {
   ];
 };
 
-export default function OtpStartPage({}: Route.ComponentProps) {
+const formSchema = z.object({
+  email: z.string().email(),
+});
+
+export const action = async ({ request }: Route.ActionArgs) => {
+  const formData = await request.formData();
+  const { success, data } = formSchema.safeParse(Object.fromEntries(formData));
+  if (!success) return { error: 'Invalid email address' };
+
+  const { email } = data;
+  const { client } = makeSSRClient(request);
+  const { error } = await client.auth.signInWithOtp({
+    email,
+    options: {
+      // 이메일 로그인 시 사용자 생성 여부
+      shouldCreateUser: true,
+    },
+  });
+  if (error) return { error: 'Failed to send OTP' };
+
+  return redirect(`/auth/otp/complete?email=${email}`);
+};
+
+export default function OtpStartPage({ actionData }: Route.ComponentProps) {
+  const navigation = useNavigation();
+  const isSubmitting =
+    navigation.state === 'submitting' || navigation.state === 'loading';
+
   return (
     <div className='flex flex-col justify-center items-center h-full relative'>
       <div className='flex flex-col justify-center items-center gap-10 w-full max-w-md'>
@@ -20,7 +50,7 @@ export default function OtpStartPage({}: Route.ComponentProps) {
             We will send you a 4-digit code to your email.
           </p>
         </div>
-        <Form className='w-full space-y-4'>
+        <Form method='post' className='w-full space-y-4'>
           <InputPair
             id='email'
             name='email'
@@ -30,10 +60,18 @@ export default function OtpStartPage({}: Route.ComponentProps) {
             placeholder='Enter your email'
             required
           />
-
-          <Button type='submit' className='w-full'>
-            Send OTP
-          </Button>
+          {actionData?.error && (
+            <p className='text-red-500 text-sm'>{actionData.error}</p>
+          )}
+          {isSubmitting ? (
+            <Button type='submit' className='w-full' disabled>
+              <Loader className='w-4 h-4 animate-spin' />
+            </Button>
+          ) : (
+            <Button type='submit' className='w-full'>
+              Send OTP
+            </Button>
+          )}
         </Form>
       </div>
     </div>
